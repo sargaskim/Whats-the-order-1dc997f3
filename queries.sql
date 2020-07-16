9168dd9e638d13a6f37f2d81abf6e929ef529a18
sargaskim <2103702@talnet.nl>
2020-07-16 19:27:41 +0200

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -1,18 +1,14 @@
-create database netland;
+use netland;
 
--- 1. Welke series hebben een award hebben gewonnen?
-SELECT id, title, has_won_awards FROM series;
--- 2. Welke series hebben een cijfer boven de 2.5?
-select id, title, rating from series where rating < 2.5;
--- 3. Welke series zijn in Nederland gemaakt én zijn Nederlands gesproken?
-select id, title, language from series where language = 'NL';
--- 4. Welke series hebben minder dan 5 seizoenen?
-select id, title, seasons from series where seasons > 5;
--- 5. Wat is het hoogste cijfer dat een serie heeft?
-select id, title, max(rating) from series;
--- 6. Welke series hebben minder dan 3 seizoenen of meer dan 20?
-select  id, title, seasons from series where seasons <3 && seasons >20;
--- 7. Welke series hebben de lettercombinatie 'Th' in hun title?
-select  id, title from series where title like '%th%';
--- 8. Welke series **niet** 3 seizoenen hebben?
-select  id, title, seasons from series where seasons != 3;
+-- 1. Welke series, gesorteerd van hoogste cijfer naar laagste cijfer, hebben een cijfer boven de 2.5?
+SELECT id, title, rating 
+FROM series where rating > 2.5 order by rating ASC;
+
+-- 2. Welke series hebben minder dan 5 seizoenen, gesorteerd van minste aantal seizoenen naar meeste aantal seizoenen?
+SELECT id, title, seasons 
+FROM series where seasons < 5 order by seasons ASC;
+
+-- 3. Welke series hebben minder dan 3 seizoenen of meer dan 20, gesorteerd op aantal seizoenen en land van herkomst?
+SELECT id, title, seasons, country
+FROM series where seasons < 3 || seasons > 20
+order by seasons ASC, country DESC;
